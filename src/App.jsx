ebc50ea16c2caabf961322c97a320d6c82338f31
rhmti01 <rahmati.business@gmail.com>
2025-06-30 19:37:03 +0330

    add react and tailwind dependencies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' text-5xl font-bold text-cyan-800 mt-10 ' >پروژه اپ فریلنسری ریکت</h1>
+    </>
+  )
+}
+
+export default App

eb22a8b54fc0dc3c5653c750c8e29356431c97a9
rhmti01 <rahmati.business@gmail.com>
2025-07-01 18:20:02 +0330

    add features based folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import './App.css'
-import './index.css'
+import React from "react";
+import "./App.css";
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./Components/Auth/Auth";
 
 function App() {
-
   return (
-    <>
-      <h1 className=' text-5xl font-bold text-cyan-800 mt-10 ' >پروژه اپ فریلنسری ریکت</h1>
-    </>
-  )
+    <Routes>
+      <Route path="/auth" element={<Auth/>}  />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Auth.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Auth() {
+  return (
+    <div>Auth</div>
+  )
+}
+
+export default Auth
